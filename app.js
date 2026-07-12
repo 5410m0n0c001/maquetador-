ee0c7dfc68eb472fb65888283affe03e47576dec
Salomon Ramirez Ortega <5410m0n.r4m1r3z@gmail.com>
2026-07-12 10:45:34 -0600

    fix(sanitize): refactor character sanitization to use strict unicode escape sequences to prevent cross-encoding failures in browsers

--- app.js
+++ app.js
@@ -1918,28 +1918,43 @@
     AppState.elements.forEach(function (e) {
       // Clean up corrupted characters caused by broken UTF-8 encoding (including half-broken 'í' variants)
       if (typeof e.name === 'string') {
-        var nameClean = e.name.trim();
-        if (nameClean === 'í rbol Decorativo' || nameClean === 'Ã rbol Decorativo' || nameClean === 'Ãrbol Decorativo' || nameClean === 'írbol Decorativo') {
-          e.name = 'Árbol Decorativo';
-        } else if (nameClean === 'í rea de Barra' || nameClean === 'Ã rea de Barra' || nameClean === 'Ãrea de Barra' || nameClean === 'írea de Barra') {
-          e.name = 'Área de Barra';
-        } else if (nameClean === 'í rea de Cocina' || nameClean === 'Ã rea de Cocina' || nameClean === 'Ãrea de Cocina' || nameClean === 'írea de Cocina') {
-          e.name = 'Área de Cocina';
-        } else {
+        var nClean = e.name.trim();
+        
+        // 1. Árbol Decorativo (\u00C1rbol)
+        if (/[\u00C3\u00EDíÃ]\s*rbol/i.test(nClean)) {
+          e.name = '\u00C1rbol Decorativo';
+        }
+        // 2. Área de Barra / Área de Cocina (\u00C1rea)
+        else if (/[\u00C3\u00EDíÃ]\s*rea/i.test(nClean)) {
+          if (nClean.indexOf('Barra') > -1) {
+            e.name = '\u00C1rea de Barra';
+          } else if (nClean.indexOf('Cocina') > -1) {
+            e.name = '\u00C1rea de Cocina';
+          } else {
+            e.name = '\u00C1rea';
+          }
+        }
+        // 3. Salón Techado (Sal\u00F3n)
+        else if (/Sal[\u00C3\u00ED\u00F3íÃ]\s*n/i.test(nClean)) {
+          if (nClean.indexOf('Arco') > -1) {
+            e.name = 'Arco Estructural/Sal\u00F3n';
+          } else {
+            e.name = 'Sal\u00F3n Techado';
+          }
+        }
+        // 4. Mesa Mármol Rectangular (M\u00E1rmol)
+        else if (/M[\u00C3\u00ED\u00E1íÃ]rmol/i.test(nClean)) {
+          e.name = 'Mesa M\u00E1rmol Rectangular';
+        }
+        // 5. General fallback cleanups
+        else {
           e.name = e.name
-            .replace(/\u00C3\s*rbol/g, 'Árbol')
-            .replace(/í\s*rbol/g, 'Árbol')
-            .replace(/\u00C3\s*rea/g, 'Área')
-            .replace(/í\s*rea/g, 'Área')
-            .replace(/Sal\u00C3\u00B3n/g, 'Salón')
-            .replace(/SalÃ³n/g, 'Salón')
-            .replace(/M\u00C3\u00A1rmol/g, 'Mármol')
-            .replace(/MÃ¡rmol/g, 'Mármol')
-            .replace(/\u00C3\u00B3/g, 'ó')
-            .replace(/\u00C3\u00A1/g, 'á')
-            .replace(/\u00C3\u00A9/g, 'é')
-            .replace(/\u00C3\u00BA/g, 'ú')
-            .replace(/\u00C3\u00B1/g, 'ñ');
+            .replace(/\u00C3\u00B3/g, '\u00F3') // ó
+            .replace(/\u00C3\u00A1/g, '\u00E1') // á
+            .replace(/\u00C3\u00A9/g, '\u00E9') // é
+            .replace(/\u00C3\u00BA/g, '\u00FA') // ú
+            .replace(/\u00C3\u00B1/g, '\u00F1') // ñ
+            .replace(/\u00C3\s*/g, '\u00ED');    // í
         }
 
         // Auto-update old marble table names from square to rectangular
